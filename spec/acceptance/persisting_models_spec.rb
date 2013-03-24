b53d29fb86449e5d098fc1cb77d21fc690d29a3e
Mathieu Lajugie <mathieul@gmail.com>
2013-03-23 22:58:12 -0700

    write first acceptance test: save and reload a model

--- spec/acceptance/persisting_models_spec.rb
+++ spec/acceptance/persisting_models_spec.rb
@@ -1,7 +1,42 @@
 require "acceptance_helper"
+require "virtus"
+
+class Task
+  include Virtus
+  attribute :title, String
+  attribute :created_at, DateTime
+end
+
+class Queue
+  include Virtus
+  attribute :name, String
+  embeds :tasks, score: ->(task) { task.created_ts } do
+    define(:count)   { |key| key.zcard }
+    define(:next_id) { |key| key.command(...read next id...) }
+  end
+end
+
+class Teammate
+  include Virtus
+  attribute :name, String
+end
+
+class Skill
+  include Virtus
+  attribute :level, Integer
+  references :queue
+  references :teammate
+end
 
 feature "Persisting models" do
+  let(:store)    { Redistent::DataStore.new(:redis, Redis, redis_config) }
+  let(:queue)    { Queue.new(name: "Masada") }
+  let(:teammate) { Teammate.new(name: "John Zorn") }
+  let(:skill)    { Skill.new(queue: queue, teammate: teammate, level: 50) }
+  let(:task)     { Task.new(title: "Alef")}
+
   scenario "Save and reload a model" do
-    expect(true).to be_true
+    store.save(queue)
+    expect(store.get(:queue, queue.id).name).to eq("Masada")
   end
 end
