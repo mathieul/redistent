1f175262604c526b4113bdf4863cdcb6d091d8d4
Mathieu Lajugie <mathieul@gmail.com>
2013-03-26 07:38:08 -0700

    update config spec to expect structs rather than plain hashes

--- spec/redistent/config_spec.rb
+++ spec/redistent/config_spec.rb
@@ -11,32 +11,36 @@ describe Redistent::Config do
 
     it "adds a model with #add_model" do
       config.add_model(:name)
-      expect(config.models[:name][:persist_attributes]).to be_true
+      expect(config.models[:name].persist_attributes).to be_true
     end
 
     it "adds a reference to a model with #references" do
       config.add_model :queue do
         references :team
       end
-      expect(config.models[:queue][:references]).to eq([{model: :team, attribute: :team_id}])
+      expect(config.models[:queue]).to have(1).references
+      reference = config.models[:queue].references.first
+      expect(reference.model).to eq(:team)
+      expect(reference.attribute).to eq(:team_id)
     end
 
     it "defines an embedded collection with #embeds" do
       config.add_model :queue do
         embeds :tasks
       end
-      expect(config.models[:queue][:collections]).to eq([
-        {type: :embedded, sort_by: false, model: :task, attribute: :task_id}
-      ])
+      expect(config.models[:queue]).to have(1).collections
+      collection = config.models[:queue].collections.first
+      expect(collection.type).to eq(:embedded)
+      expect(collection.sort_by).to be_false
+      expect(collection.model).to eq(:task)
+      expect(collection.attribute).to eq(:task_id)
     end
 
     it "defines a sorted embedded collection with #embeds with a sort_by option" do
       config.add_model :queue do
         embeds :tasks, sort_by: :queued_at
       end
-      expect(config.models[:queue][:collections]).to eq([
-        {type: :embedded, sort_by: :queued_at, model: :task, attribute: :task_id}
-      ])
+      expect(config.models[:queue].collections.first.sort_by).to eq(:queued_at)
     end
 
     it "defines methods on an embedded collection with #define" do
@@ -46,7 +50,7 @@ describe Redistent::Config do
           define(:testing) { |key| called_with = key }
         end
       end
-      config.models[:queue][:collections].first[:methods][:testing].call(:test)
+      config.models[:queue].collections.first.methods[:testing].call(:test)
       expect(called_with).to eq(:test)
     end
 
@@ -54,10 +58,13 @@ describe Redistent::Config do
       config.add_model :queue do
         collection :teammates, via: :skills
       end
-      expect(config.models[:queue][:collections]).to eq([{
-        type: :referenced, model: :skill, attribute: :skill_id,
-        target: :teammate, target_attribute: :teammate_id
-      }])
+      expect(config.models[:queue]).to have(1).collections
+      collection = config.models[:queue].collections.first
+      expect(collection.type).to eq(:referenced)
+      expect(collection.model).to eq(:skill)
+      expect(collection.attribute).to eq(:skill_id)
+      expect(collection.target).to eq(:teammate)
+      expect(collection.target_attribute).to eq(:teammate_id)
     end
   end
 
@@ -65,7 +72,7 @@ describe Redistent::Config do
     it "can access hooks within each model definition" do
       config.add_hook(:before_write, :run_me)
       config.add_model(:model_name)
-      expect(config.models[:model_name][:hooks]).to eq(before_write: [:run_me])
+      expect(config.models[:model_name].hooks[:before_write]).to eq([:run_me])
     end
   end
 end
