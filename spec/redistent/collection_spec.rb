4f5ad6aa61a47e3404a8ce6de76582b130468536
Mathieu Lajugie <mathieul@gmail.com>
2013-04-04 20:20:32 -0700

    sketch out collection specs

--- spec/redistent/collection_spec.rb
+++ spec/redistent/collection_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe Redistent::Collection do
-  pending
+  context "referenced collection" do
+    it "can count the number of referrers"
+    it "can return all the referrers"
+  end
+
+  context "indirect referenced collection" do
+    it "can count the number of referrers"
+    it "can return all the referrers"
+  end
+
+  context "embedded collection" do
+    it "can run custom methods"
+  end
 end
