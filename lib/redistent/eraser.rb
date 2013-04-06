df125bc82dfcda939d015225c0b21dc7d9ed16be
Mathieu Lajugie <mathieu@caring.com>
2013-04-06 16:54:08 -0700

    simplify Eraser#erase

--- lib/redistent/eraser.rb
+++ lib/redistent/eraser.rb
@@ -17,10 +17,20 @@ module Redistent
       attribute_key(model).del
       attributes = model.persisted_attributes
       describe(model).references.each do |reference|
-        if (ref_uid = attributes[reference.attribute])
-          index_key(model, reference.attribute)[ref_uid].srem(model.uid)
-          reference_key(model, reference.attribute).del
-          sorted = sorted_key(reference.model, ref_uid, reference.collection.attribute)
+        cleanup_indices(model, reference)
+      end
+    end
+
+    private
+
+    def cleanup_indices(model, reference)
+      return if model.persisted_attributes.nil?
+      if (ref_uid = model.persisted_attributes[reference.attribute])
+        index_key(model, reference.attribute)[ref_uid].srem(model.uid)
+        reference_key(model, reference.attribute).del
+        collection = reference.collection
+        if collection.type == :sorted
+          sorted = sorted_key(reference.model, ref_uid, collection.attribute)
           sorted.zrem(model.uid)
         end
       end
